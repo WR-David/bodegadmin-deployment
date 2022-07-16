0858ac72bc06971352918cae8042b9a8fcd68fe3
David <david.riveraf21@gmail.com>
2022-07-16 12:51:27 -0400

    Estructura completa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+// import Login from './components/Login/Login.jsx'
+import Home from './pages/Home/home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [connected, setConnected] = useState(true)
+
+	const login = (state) => {
+		setConnected(state)
+	}
+
+	return (
+		// connected ? <Home /> : <Login login={login} />
+		connected ? <Home /> : <Home login={login} />
+	)
 }
-
-export default App;
